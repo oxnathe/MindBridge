500928009322d094a3476334600b414c1fc5df0f
OluwatomisinDev <oluwatomisindan@gmail.com>
2025-10-29 10:53:39 +0100

    Assets loading
    Uploaded fonts, Images, Created Theme.js files

--- MindBridge/app/_layout.jsx
+++ MindBridge/app/_layout.jsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
-  return <Stack />;
+
+  const [loaded, error] = useFonts({
+    serifRegular: require('../assets/fonts/DM_Serif_Display/DMSerifDisplay-Regular.ttf'),
+    poppinsRegular: require('../assets/fonts/Poppins/Poppins-Regular.ttf'),
+    poppinsMedium: require('../assets/fonts/Poppins/Poppins-Medium.ttf'),
+    poppinsSemiBold: require('../assets/fonts/Poppins/Poppins-SemiBold.ttf'),
+    poppinsLight: require('../assets/fonts/Poppins/Poppins-Light.ttf'),
+    poppinsBold: require('../assets/fonts/Poppins/Poppins-Bold.ttf'),
+  })
+
+
+  return (
+    <Stack screenOptions={{headerShown: false}}>
+      <Stack.Screen name="index"/>
+    </Stack>
+  );
 }
---
+++ MindBridge/themes/theme.js
@@ -0,0 +1,21 @@
+
+
+
+const COLORS = {
+    backgroundColor: '#EFF6FF',
+    textColor: '#031633',
+    neutralColor: "#404040",
+    white: "#FFFFFF",
+    skipColor: "#031633",
+    buttonColor: "#0057B2",
+    btnColor: "#084298",
+    formText: "#404040"
+}
+
+const SIZES = {
+    largerXL: 32,
+    large: 26,
+    medium: 24,
+    regular: 18,
+    small: 16,
+}
