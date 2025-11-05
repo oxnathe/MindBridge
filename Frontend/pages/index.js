58c63ca91e4f4e2a2ce1341b0658d32b4649eae0
Honor-MB <adeabdulmuizz@gmail.com>
2025-11-05 08:35:13 +0100

    update

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,6 +1,6 @@
 
   const menuToggle = document.querySelector('.header__menu');
-  const navMenu = document.querySelector('.header__nav');
+  const navMenu = document.querySelector('.menu__nav');
 
   menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
