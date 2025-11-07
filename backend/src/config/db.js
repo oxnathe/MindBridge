b519eb92fb3ad24149856ac98d0e5d022d2d7c34
Derrbbyy <onuohdeborah23@gmail.com>
2025-11-07 10:44:37 +0100

    updated db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,15 +3,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const sequelize = new Sequelize(
-    process.env.DB_NAME || 'mindbridge',
-    process.env.DB_USER || 'root',
-    process.env.DB_PASSWORD || '',
+    process.env.DATABASE_URL,
+    
+    // process.env.DB_NAME || 'mindbridge',
+    // process.env.DB_USER || '',
+    // process.env.DB_PASSWORD || '',
 
 
     {
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || '3306',
+        // host: process.env.DB_HOST || '',
+        // port: process.env.DB_PORT || '',
         dialect: 'postgres',
+        dialectOptions:{
+            ssl:{
+                require:true,
+                rejectUnauthorized:false
+            }
+        },
         logging:false
 
     }
